f86c5033f62cc882f761d8475b233798bf9af072
kornevVlad <vlad1990@yandex.ru>
2024-04-25 11:54:53 +0300

    добавлены контроллеры car/driver/user

---
+++ drivers-registration-java/src/main/java/com/example/driversregistrationjava/controller/DriverController.java
@@ -0,0 +1,12 @@
+package com.example.driversregistrationjava.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/drivers")
+@RequiredArgsConstructor
+public class DriverController {
+}
